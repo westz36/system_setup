92e890510064177d44756c0f1c7a8bf310497f87
Westz36 <westz36@users.noreply.github.com>
2018-06-24 00:10:26 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,9 @@ sudo apt-get update
 
 sudo apt-get dist-upgrade
 
-sudo apt install curl git openssh-server mongodb net-tools npm
+chmod +x ~./run.sh
+
+sudo apt install curl git openssh-server mongodb net-tools npm python-pip
+
+sudo pip install shodan
+
